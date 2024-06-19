ed1b5e200851c797dbeccc4c2bfa89d66b108700
Mau <mauvepinktree@gmail.com>
2024-06-18 22:19:01 -0300

    add glad

--- src/core.c
+++ src/core.c
@@ -2,28 +2,34 @@
 
 #include <stdio.h>
 
-bool initRenderer(){
+bool initRenderer() {
     if (!glfwInit()) {
         printf("(Error) Couldn't initialize GLFW context!\n");
         return false;
     }
     else {
         printf("Initialized GLFW successfully!\n");
-        return true;
     }
+    return true;
 }
 
-void stopRenderer(){
+void stopRenderer() {
     //unloadDefaultShaders();
     glfwTerminate();
     printf("Terminated GLFW context!\n");
 }
 
-bool initGlad(){
-    printf("Initing GLAD...\n");
+bool initGlad() {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "Failed to initialize GLAD\n");
+        return false;
+    }
+    else {
+        printf("Initialized GLAD successfully!\n");
+    }
     return true;
 }
 
-void pollRenderer(){
+void pollRenderer() {
     glfwPollEvents();
 }
