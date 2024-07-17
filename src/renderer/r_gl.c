4ed4e9cb29e54832edba93f0ac70bff19f2ba0d9
Mau <mauvepinktree@gmail.com>
2024-07-17 18:40:15 -0300

    add alpha channel

--- src/renderer/r_gl.c
+++ src/renderer/r_gl.c
@@ -4,13 +4,13 @@
 
 void rglSetUpDefaultVertexAttributes() {
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
     // uv attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(7 * sizeof(float)));
     glEnableVertexAttribArray(2);
 }
 
@@ -50,6 +50,9 @@ RenderData rglCreateRenderData(const float vertexData[], size_t vertexDataSize, 
     glBindBuffer(GL_ARRAY_BUFFER, rData.VBO);
     rglSetUpDefaultVertexAttributes();
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     printf("Render data created! VAO: %i, VBO: %i, EBO: %i.\n", rData.VAO, rData.VBO, rData.EBO);
 
